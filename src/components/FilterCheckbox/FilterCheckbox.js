747f177921d1c3ac58d564070660a40d3d8b97fe
Valeriya Zolotaryova <Panda.lera@mail.ru>
2021-03-04 20:27:47 +0500

    fixed adaptive bags

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -4,7 +4,7 @@ export default function FilterCheckbox(props) {
   return (
     <div className="filter-button__container">
       <input type="checkbox" name={props.name} id={props.name} className="filter-button__input"/>
-      <label for={props.name} className="filter-button__text">{props.text}</label>
+      <label htmlFor={props.name} className="filter-button__text">{props.text}</label>
     </div>
   )
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,22 @@
-  
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderNavigationMobile from '../HeaderNavigationMobile/HeaderNavigationMobile';
 import HeaderNavigationMain  from '../HeaderNavigationMain/HeaderNavigationMain';
 
 export default function NavBar(){
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const updateWindowSize = () => setWindowWidth(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWindowSize);
+        return () => window.removeEventListener('resize', updateWindowSize);
+    });
+
+
     return(
         <>
             {
-            (window.innerWidth > 768) ? <HeaderNavigationMain /> : <HeaderNavigationMobile/>
+            (windowWidth > 768) ? <HeaderNavigationMain /> : <HeaderNavigationMobile/>
             }
         </>
     )
